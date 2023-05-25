43a1b3b0c26e06f09153ffa5f0fce25f1a54c7bb
Bondith Sovann <bondithsovann004@gmail.com>
2023-05-24 23:56:42 -0500

    Update: Level 1 Security Completed.

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const encrypt = require('mongoose-encryption');
 const bodyParser = require('body-parser'); 
 
 // connect mongodD with Mongoose
+main().catch(err => console.log(err));
+async function main(){
+        await mongoose.connect("mongodb://127.0.0.1:27017/User");
+}
+// Define Schema 
+const userSchema = {
+    email : String,
+    password : String
+};
+
+// Initialize collection
+const User = new mongoose.model('User', userSchema);
+
 // Initialize App with Express
 const app = new express(); 
 
@@ -27,6 +40,26 @@ app.get("/login", function(req, res){
 app.get("/register", function(req, res){
     res.render("register");
 }); 
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email : req.body.username, 
+        password : req.body.password
+    });
+    newUser.save();
+    res.render("secrets");
+}); 
+
+app.post("/login", async function(req, res){
+    const email = req.body.username; 
+    const password = req.body.password; 
+
+    const userInfo = await User.findOne({email : email, password : password});
+    if (userInfo != null)
+        res.render("secrets");
+    else 
+        res.render("login"); // Password Incorrect;
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000!");
