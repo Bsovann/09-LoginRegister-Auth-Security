30175956a3554724976cec1246c67fa3c578260d
Bondith Sovann <bondithsovann004@gmail.com>
2023-06-02 21:57:02 -0500

    Add: passport cookie and session

--- app.js
+++ app.js
@@ -5,6 +5,35 @@ const mongoose = require('mongoose');
 const ejs = require('ejs'); 
 const bodyParser = require('body-parser'); 
 const md5 = require('md5');
+const session = require('express-session'); 
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+/*
+    1. Set up express session
+    2. Initialize passport and use it
+*/
+
+// Initialize App with Express
+const app = new express(); 
+
+// Deploy ejs
+app.set("view engine", "ejs");
+// Deploy Body-parser 
+app.use(bodyParser.urlencoded({extended : true})); 
+// Express
+app.use(express.static("public"));
+// Express session : Don't foget to read through the docs
+app.use(session({
+    secret : "My Litter Secret!",
+    resave : false, 
+    saveUninitialized : false
+}));
+// Initialize passport
+app.use(passport.initialize());
+
+// Tell app to use passport to deal with session : read docs on configure section
+app.use(passport.session()); 
 
 // connect mongodD with Mongoose
 main().catch(err => console.log(err));
@@ -18,18 +47,16 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
+// Set up passport-local-mongoose
+userSchema.plugin(passportLocalMongoose); // do all the salt and hash
+
 // Initialize collection
 const User = new mongoose.model('User', userSchema);
 
-// Initialize App with Express
-const app = new express(); 
-
-// Deploy ejs
-app.set("view engine", "ejs");
-// Deploy Body-parser 
-app.use(bodyParser.urlencoded({extended : true})); 
-// Express
-app.use(express.static("public"))
+// passport config with mongodb 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -43,30 +70,55 @@ app.get("/register", function(req, res){
     res.render("register");
 }); 
 
-app.post("/register", function(req, res){
-    const newUser = new User({
-        email : req.body.username, 
-        password : md5(req.body.password)
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated())
+        res.render("secrets");
+    else
+        res.redirect("/login");
+});
+
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if(err)
+            console.log(err);
+        else
+            res.redirect("/");
     });
-    newUser.save();
-    res.render("secrets");
+});
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    }); 
 }); 
 
 app.post("/login", async function(req, res){
-    const email = req.body.username; 
-    const password = md5(req.body.password); 
+    const user = new User({
+        username : req.body.username, 
+        password : req.body.password
+    });
 
-    const userInfo = await User.findOne({email : email});
-
-    if (userInfo != null){
-            if(userInfo.password === password)
-                res.render("secrets");
-            else
-                res.render("login");
-    }
-    else 
-        res.render("login"); // Password Incorrect;
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+   });
 });
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000!");
