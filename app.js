5cb3d8bf7d987d5dbf56ce1bb3142f5af8603c1a
Bondith Sovann <102501224+Bsovann@users.noreply.github.com>
2023-06-02 22:52:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //jshint esversion:6
 require('dotenv').config();
 const express = require('express');
@@ -123,7 +123,7 @@ app.post("/login", async function(req, res){
 
 app.listen(3000, function(){
     console.log("Server started on port 3000!");
-=======
+
 //jshint esversion:6
 require('dotenv').config();
 const express = require('express');
@@ -197,5 +197,4 @@ app.post("/login", async function(req, res){
 
 app.listen(3000, function(){
     console.log("Server started on port 3000!");
->>>>>>> 849b3d0 (Add: add dependencies)
 });
